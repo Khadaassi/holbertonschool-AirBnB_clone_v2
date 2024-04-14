766e444bdcf64ece76b1bf82ed8b97de46850960
Khadaassi <aassikhadija@yahoo.fr>
2024-04-14 15:30:47 +0200

    Refactor all method in FileStorage class to filter objects by class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,15 +9,15 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """Returns the list of objects of one type of class or all objects"""
-        #if cls is none -> retrieve all objects (return dict)
+        """Returns a dictionary of models currently in storage"""
         if cls is None:
             return FileStorage.__objects
-        #If an args is given when calling the method
         else:
-            #Return filtred list depended on given class
-            return {key: obj for key, obj in FileStorage.__objects.items()
-                    if isinstance(obj, cls)}    
+            obj = {}
+            for key, value in FileStorage.__objects.items():
+                if cls.__name__ in key:
+                    obj[key] = value
+            return obj
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -52,17 +52,14 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """ Deletes an object from the storage dict """
-        if obj is None:
-            return
-        else:
-            #Create a key for the object to delete
-            key = obj.to_dict()['__class__'] + '.' + obj.id
-            #If key exist in dictionnary, delete key -> delete object
+        """Deletes obj from __objects if it’s inside"""
+        if obj:
+            key = obj.__class__.__name__ + '.' + obj.id
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
+            self.save()
